32030482856cc0c1815c3c6dd424f48d535f3a4f
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-03-10 13:33:23 +0530

    Time: 7 ms (6.93%), Space: 7.2 MB (5.57%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,121 @@
+class Solution {
+public:
+    void recurse(string ip, string op, vector<string>& ans)
+    {
+        if(ip.size()==0)
+        {
+            if(op.size()!=0)
+            ans.push_back(op);
+            return;
+        }
+        if(ip[0]=='2')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('a');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('b');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('c');
+            recurse(ip,op,ans);  
+        }
+       else  if(ip[0]=='3')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('d');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('e');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('f');
+            recurse(ip,op,ans);  
+        }
+       else  if(ip[0]=='4')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('g');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('h');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('i');
+            recurse(ip,op,ans);  
+        }
+       else  if(ip[0]=='5')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('j');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('k');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('l');
+            recurse(ip,op,ans);  
+        }
+       else  if(ip[0]=='6')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('m');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('n');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('o');
+            recurse(ip,op,ans);  
+        }
+        else if(ip[0]=='7')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('p');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('q');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('r');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('s');
+            recurse(ip,op,ans);
+        }
+       else  if(ip[0]=='8')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('t');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('u');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('v');
+            recurse(ip,op,ans);  
+        }
+       else  if(ip[0]=='9')
+        {
+            ip.erase(ip.begin()+0);
+            op.push_back('w');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('x');
+            recurse(ip,op,ans);
+            op.erase(op.end()-1);
+            op.push_back('y');
+            recurse(ip,op,ans);  
+            op.erase(op.end()-1);
+            op.push_back('z');
+            recurse(ip,op,ans);
+       }
+         return;
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        string op="";
+        recurse(digits,op,ans);
+        return ans;
+    }
+};
