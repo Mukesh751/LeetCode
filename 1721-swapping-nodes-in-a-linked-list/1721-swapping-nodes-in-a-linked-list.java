a9bb12b3fc48311504bc7fc1e0477119d7e61994
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-04-04 12:55:10 +0530

    Time: 2 ms (100.00%), Space: 168.2 MB (70.11%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -15,28 +15,18 @@ class Solution {
         ListNode end=head;
         int t=k;
         t--;
-        int n=1;
-        while(current.next!=null)
-        {
-            current=current.next;
-            n++;
-        }
-        current=head;
         while(t>0)
         {
             t--;
             current=current.next;
         }
         start=current;
-        t=n-k;
-        current=head;
-        System.out.print(t);
-        while(t>0)
+        while(current.next!=null)
         {
-            t--;
+            end=end.next;
             current=current.next;
         }
-        end=current;
+        
         int temp = start.val;
         start.val = end.val;
         end.val = temp;
