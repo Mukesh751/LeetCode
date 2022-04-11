3a077552fa44d989290a4a10f699cd2d0fd5c00e
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-04-12 01:00:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (98.16%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    int dp[101][10001];
-    int solve(int e, int f)
-    {
-        if(f==0 || f==1)
-            return f;
-        if(e==1)
-            return f;
+//     int dp[101][10001];
+//     int solve(int e, int f)
+//     {
+//         if(f==0 || f==1)
+//             return f;
+//         if(e==1)
+//             return f;
         
-        if(dp[e][f]!=-1)
-            return dp[e][f];
+//         if(dp[e][f]!=-1)
+//             return dp[e][f];
         
-      int mn=INT_MAX;
-    int start=1,end=f;
-    while(start<=end)
-    {
-        int mid=start+(end-start)/2;
-        int low=solve(e-1,mid-1);
-        int high=solve(e,f-mid);
-        int temp=1+max(low,high);
-        mn=min(mn,temp);
+//       int mn=INT_MAX;
+//     int start=1,end=f;
+//     while(start<=end)
+//     {
+//         int mid=start+(end-start)/2;
+//         int low=solve(e-1,mid-1);
+//         int high=solve(e,f-mid);
+//         int temp=1+max(low,high);
+//         mn=min(mn,temp);
         
-        if(high>low)
-            start=mid+1;
-        else end=mid-1;
-    }
-        return dp[e][f]=mn;
-    }
+//         if(high>low)
+//             start=mid+1;
+//         else end=mid-1;
+//     }
+//         return dp[e][f]=mn;
+//     }
     int superEggDrop(int k, int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(k,n);
+        //memset(dp, -1, sizeof(dp));
+         vector<int> dp(k+1);
+          int step = 0;
+          for (; dp[k] < n; step++) {
+            for (int i = k; i > 0; i--)
+                dp[i] = (1 + dp[i] + dp[i-1]);
+        }
+        return step;
     }
 };
