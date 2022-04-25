d0dd5218ed530baa98f6eca3cce3bb47bdb085d9
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-04-25 16:18:57 +0530

    Time: 13 ms (58.54%), Space: 10.8 MB (36.98%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,74 +1,15 @@
 class Solution {
 public:
-    char v[1005][5000];
-    bool check = true;
-    void fillcol(string s, int rows, int col, int j)
-    {
-        for(int i=0; i<rows ; i++)
-        {
-            if(j==s.size())
-            {
-                check = false;
-                break;
-            }
-            v[i][col]=s[j];
-            j++;
-        }
-        if(check == true)
-            filldiag(s,rows,col,j);
-    }
-      void filldiag(string s, int rows, int col, int j)
-    {
-        for(int i=rows-2; i>0 ; i--)
-        {
-            if(j==s.size())
-            {
-                check = false;
-                break;
-            }
-            col++;
-            v[i][col]=s[j];
-            j++;
-        }
-          col++;
-        if(check == true)
-            fillcol(s,rows,col,j);
-    }
+   
     string convert(string s, int numRows) {
-        
-        if(numRows==1)
-            return s;
-        
-        int n = s.size();
-        for(int i=0; i<numRows+1; i++)
-            for(int j=0; j<4000; j++)
-                v[i][j]='$';
-        
-        fillcol(s,numRows,0,0);
-        string str;
-        
-        for(int i=0; i<numRows+1; i++)
-           for(int j=0; j<4000; j++)
-              if(v[i][j]!='$')  
-               str.push_back(v[i][j]);
-        
-       //  char ch;
-       // for(int i=0; i<str.size(); i++)
-       // {
-       //     if(str[i]>='a' && str[i]<='z')
-       //         continue;
-       //    else if(str[i]>='A' && str[i]<='Z')
-       //         continue;
-       //     else if(str[i]==',' || str[i]=='.')
-       //         continue;
-       //     else
-       //         ch=str[i];
-       // }
-//         string ans="";
-//         for(int i=0; i<str.size(); i++)
-//             if(str[i]!=ch)
-//                 ans.push_back(str[i]);
-        
-        return str;
+        string ans;
+        vector <string> tmp(numRows);                                                         
+        int i=0, n=s.size();
+        while(i<n){                                      
+            for(int j=0;j<numRows && i<n; j++, i++) tmp[j] += s[i];               
+            for(int j=numRows - 2;j>0 && i<n; j--, i++) tmp[j] += s[i];            
+        }
+        for(int j=0;j<numRows; j++) ans += tmp[j];                                       
+        return ans;
     }
 };
