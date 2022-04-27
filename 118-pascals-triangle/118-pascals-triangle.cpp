ac2bdc8c0bca5ccef3e474d82da3bdfeb9608c2a
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-04-27 12:32:27 +0530

    Time: 0 ms (100.00%), Space: 7 MB (33.55%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void solve(int r, int k)
+    {
+        if(k>r-1)
+            return;
+        
+        vector<int> temp;
+        int sum = 1;
+        
+        if(k==0)
+            temp.push_back(sum);
+        else
+        {
+            temp.push_back(sum);
+            for(int i=0; i<ans[k-1].size()-1; i++)
+            {
+                sum = ans[k-1][i] + ans[k-1][i+1];
+                temp.push_back(sum);
+            }
+            sum = 1;
+            temp.push_back(sum);
+        }
+     
+        ans.push_back(temp);
+        solve(r,k+1);
+        return;
+    }
+    
+    vector<vector<int>> generate(int numRows) {
+        solve(numRows,0);
+        return ans;
+    }
+};
