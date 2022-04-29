9886d8cc53498ab8de8f3264c11da1800c67e8b6
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-04-29 14:23:05 +0530

    Time: 3 ms (52.90%), Space: 7.8 MB (84.42%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+   int dp[105];
+    int ans = 0;
+    int solve(int i,int n,vector<int> &nums ){
+
+    if(n==i)
+    {
+        return nums[i];
+    }
+    if(n<i)
+    return 0;
+
+    if(dp[n]!=-1)
+    return dp[n];
+
+     int pick =nums[n]+solve(i,n-2,nums);
+     int notpick=0+solve(i,n-1,nums);
+
+        cout<<pick<<" "<<notpick<<endl;
+    return dp[n]=max(pick,notpick);  
+}
+   
+     
+    
+    
+    int rob(vector<int>& nums) {
+       
+        if(nums.size()==1)
+            return nums[0];
+        
+        memset(dp,-1,sizeof(dp));
+      // if(nums.size()%2==1)
+      //   return solveodd(nums.size()-1,nums);
+        
+        int l = solve(0,nums.size()-2,nums);
+        memset(dp,-1,sizeof(dp));
+        
+        int r = solve(1,nums.size()-1,nums);
+        
+        cout<<l<<" "<<r;
+        return max(l,r);
+        //return r;
+    }
+};
