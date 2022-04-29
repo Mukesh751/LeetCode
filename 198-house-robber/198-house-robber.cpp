d7a20e7e5ed53e887a5d794700a301a4049a8e21
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-04-29 13:26:45 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (5.42%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    map<int,int> dp;
+    int ans = 0;
+    int solve(int n,vector<int> &nums ){
+
+    if(n==0)
+    return nums[n];
+   
+    if(n<0)
+    return 0;
+
+    if(dp.find(n)!=dp.end())
+    return dp[n];
+
+     int pick =nums[n]+solve(n-2,nums);
+     int notpick=0+solve(n-1,nums);
+
+    return dp[n]=max(pick,notpick);  
+}
+    int rob(vector<int>& nums) {
+       // memset(dp,-1,sizeof(dp));
+        return solve(nums.size()-1,nums);
+    }
+};
