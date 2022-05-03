f5ae03d3d773e0892657415372d1b0c1351103de
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-03 13:44:13 +0530

    Time: 56 ms (24.24%), Space: 26.5 MB (89.65%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -5,24 +5,15 @@ public:
     
     int findUnsortedSubarray(vector<int>& nums) {
         
-          vector<int> v(nums.begin(),nums.end());
-        sort(v.begin(),v.end());
-        int start=-1;
-        int end=-1;
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i]==v[i])
-                continue;
-            
-            if(start==-1)
-                start=i;
-            else
-                end=i;
+      int start = -1, end = -1,  max = INT_MIN;
+        for(int i=0; i<nums.size();i++){
+            if(max>nums[i]){
+                if(start == -1)start = i-1;
+                while(start-1>=0 && nums[start-1]>nums[i])start--;
+                end = i+1;
+            }
+            else max = nums[i];
         }
-        cout<<start<<" "<<end;
-        if(start==-1 && end==-1)
-            return 0;
-        
-        return end-start+1;
+        return end - start;   
     }
 };
