4f41006531791c17e931ae8f7d77787ad7dc403c
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-10 02:42:06 +0530

    Time: 3 ms (43.75%), Space: 7.2 MB (43.14%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -3,21 +3,16 @@ public:
     double average(vector<int>& nums) {
         int mn = INT_MAX;
         int mx = INT_MIN;
+        double ans = 0;
         for(int x : nums)
         {
             mn = min(mn,x);
             mx = max(mx,x);
+            ans+=x;
         }
-        double ans = 0;
-        int count = 0;
-        for(int x : nums)
-        {
-            if(x!=mn && x!=mx)
-            {
-                ans += x;
-                count++;
-            }
-        }
+        ans-=(mn+mx);
+        int count = nums.size()-2;
+        
         return ans/count;
     }
 };
