eddca2ed1bc771a97d23eefdcd10fd1e17885a26
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-12 16:02:02 +0530

    Time: 22 ms (35.36%), Space: 11.1 MB (25.58%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-      vector<vector<int>> ans;
-          set<vector<int>> s;
-        sort(nums.begin(),nums.end());
-        do{
-            if(s.find(nums)!=s.end())
-                continue;
+        vector<vector<int>> ans;
+
+    void solve(vector<int> nums, int index) {
+        if (index == nums.size()) {
             ans.push_back(nums);
-            s.insert(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
+            return;
+        }
+
+        set<int> s;
+        for (int i = index; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end())    
+                continue;
+            s.insert(nums[i]);
+            swap(nums[index], nums[i]);
+            solve(nums, index + 1);
+            swap(nums[index], nums[i]);
+        }
+        return;
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+      // vector<vector<int>> ans;
+      //     set<vector<int>> s;
+      //   sort(nums.begin(),nums.end());
+      //   do{
+      //       if(s.find(nums)!=s.end())
+      //           continue;
+      //       ans.push_back(nums);
+      //       s.insert(nums);
+      //   }while(next_permutation(nums.begin(),nums.end()));
+        solve(nums,0);
         return ans;
     }
 };
