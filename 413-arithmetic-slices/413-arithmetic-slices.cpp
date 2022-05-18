69ae91351666488d5f671a872f45334d2b8d101d
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-19 03:48:37 +0530

    Time: 2 ms (63.11%), Space: 7.4 MB (50.98%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-   
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size() <= 2)return 0;
-        int count = 0, ans = 0;
-        for(int i = 0; i < nums.size() - 2; i++){
-            if(nums[i+1] - nums[i] == nums[i+2] - nums[i+1]){
-                count++;
-            }
-            else{
-                ans += ((count * (count+1)) / 2);
-                count = 0;
-            }
+    
+int func(vector<int> &nums,int diff,int idx){
+        if(idx >= nums.size()) 
+            return 0;
+        if(nums[idx]-nums[idx-1] == diff)
+            return 1 + func(nums,diff,idx+1);
+        else 
+            return 0; 
+    }
+    int numberOfArithmeticSlices(vector<int>& nums){
+        
+        if(nums.size() == 1 || nums.size() == 2) 
+            return 0;
+        
+        int ans = 0;
+        
+        for(int i=0;i<=nums.size()-3;i++){
+            
+            int diff = nums[i+1] - nums[i];
+            
+            if(nums[i+2]-nums[i+1] == diff && nums[i+1]-nums[i] == diff)
+                ans += 1 + func(nums,diff,i+3);
             
         }
-        if(count) ans += ((count * (count+1)) / 2);
         return ans;
     }
-    
 };
