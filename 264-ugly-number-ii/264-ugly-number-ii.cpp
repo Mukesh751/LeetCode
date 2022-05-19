6ff0d644507c61753c0381e8e8804a62cdead5a0
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-20 04:19:14 +0530

    Time: 201 ms (12.43%), Space: 32.2 MB (9.72%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -2,35 +2,23 @@ class Solution {
 public:
    
     int nthUglyNumber(int n){
-        vector<unsigned long long int> v;
-        set<unsigned long long int> s;
-        v.push_back(1);
-        s.insert(1);
-        long long int i = 0;
-        while(v.size() <min(n*4,n*3 + 500)){
-           unsigned long long int k = v[i];
-            if(s.find(2*k) == s.end())
-            {
-                v.push_back(2*k);
-                s.insert(2*k);
-            }
-             if(s.find(3*k) == s.end())
-            {
-                v.push_back(3*k);
-                s.insert(3*k);
-            }
-             if(s.find(5*k) == s.end())
-            {
-                v.push_back(5*k);
-                s.insert(5*k);
-            }
-            i++;
-        }
-        for(int x : v)
-          cout<<x<<" ";
-        sort(v.begin(),v.end());
-        // for(int x : v)
-        //     cout<<x<<" ";
-        return v[n-1];
+     priority_queue<long long, vector<long long>, greater<long long>> minhp;
+    minhp.push(1);
+    map<long long,int> mp;
+    int cnt = 0;
+    long long ans;
+    mp[1] =1;
+    while(cnt < n){
+        ans = minhp.top();
+        minhp.pop();cnt++;
+        if(mp[ans*2] == 0)
+            minhp.push(ans*2),mp[ans*2]=1;
+        if(mp[ans*3] == 0)
+            minhp.push(ans*3),mp[ans*3]=1;
+        if(mp[ans*5] == 0)
+            minhp.push(ans*5),mp[ans*5]=1;
     }
+    return ans;
+}
+    
 };
