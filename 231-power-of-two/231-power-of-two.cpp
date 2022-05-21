d50bd2e7f92bb9adf6b85ebfacb2a3026accde38
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-22 01:29:16 +0530

    Time: 6 ms (15.89%), Space: 5.9 MB (25.79%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,24 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(long long int n) {
         
-         if(n==1)
-            return true;
-        
-        if(n<=0 || n%2!=0 )
-            return false;
-        
-       double x = log2(n);
-        
-        if(floor(x) == x)
-            return true;
-        
-        // for(long long int i=1; i<=n; i=i*2)
-        //     if(i==n)
-        //         return true;
-        
-       
-       //return isPowerOfTwo(n/2);
-        return false;
+       return n > 0 && not (n & n - 1);
         
        
     }
