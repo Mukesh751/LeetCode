40af62917caaf93f10af44f7f754a6197df4a010
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-05-28 20:13:22 +0530

    Time: 10 ms (82.52%), Space: 8.8 MB (64.79%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,8 +2,9 @@ class Solution {
         
 public:
     
-vector<vector<int>>mem;
-    int solve(string &word1,string &word2,int i,int j){
+    vector<vector<int>>mem;
+    string word1,word2;
+    int solve (int i,int j){
     
         if(i>=word1.size() and j>=word2.size())
             return 0;
@@ -14,16 +15,18 @@ vector<vector<int>>mem;
         if(mem[i][j]!=-1)
             return mem[i][j];
         if(word1[i]==word2[j])
-                return mem[i][j]=solve(word1,word2,i+1,j+1);
+                return mem[i][j]=solve(i+1,j+1);
         
-        int insert=solve(word1,word2,i,j+1);
-        int replace=solve(word1,word2,i+1,j+1);
-        int del=solve(word1,word2,i+1,j);
+        int insert=solve(i,j+1);
+        int replace=solve(i+1,j+1);
+        int del=solve(i+1,j);
         return mem[i][j]=1+min(insert,min(del,replace));
         }
 
     int minDistance(string &word1, string &word2) {
+        this->word1 = word1;
+        this->word2 = word2;
         mem=vector<vector<int>>(word1.size(),vector<int>(word2.size(),-1));
-        return solve(word1,word2,0,0);
+        return solve(0,0);
     }
 };
