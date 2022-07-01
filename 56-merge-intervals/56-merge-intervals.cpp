a34f1450b183809aefa822b2bb341f0cc0f35eb1
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-07-01 10:22:17 +0530

    Time: 55 ms (55.06%), Space: 19.5 MB (42.89%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-          vector<vector<int>> ans;
-        sort(intervals.begin(), intervals.end());
-        vector<int> temp;
-        temp.push_back(intervals[0][0]);
-        int prev = intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            
-            if(prev >= intervals[i][0]){
-                prev = max(intervals[i][1], prev);
-            }
-            else{
-                temp.push_back(prev);
-                ans.push_back(temp);
-                temp.clear();
-                temp.push_back(intervals[i][0]);
-                prev = intervals[i][1];
-            }
-            
+    vector<vector<int>> merge(vector<vector<int>>& que) {
+       int n=que.size();
+vector<vector<int>>ans;
+sort(que.begin(),que.end());
+
+    ans.push_back({que[0][0],que[0][1]});
+    for(int i=1;i<n;i++)
+    {
+        int s1=ans.back()[0];
+        int e1=ans.back()[1];
+        int s2=que[i][0];
+        int e2=que[i][1];
+        if(s2>e1)
+        {
+            ans.push_back({que[i][0],que[i][1]});
         }
-        temp.push_back(prev);
-        ans.push_back(temp);
-        return ans;
+        else
+        {
+            e1=max(e1,e2);
+            ans.pop_back();
+            ans.push_back({s1,e1});
+        }
+    }
+    return ans;
     }
 };
