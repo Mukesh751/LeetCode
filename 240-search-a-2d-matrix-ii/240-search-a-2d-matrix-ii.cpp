c838bd41a54390fade50d1ff0b790e6d1d502b2d
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-07-03 23:35:14 +0530

    Time: 690 ms (5.00%), Space: 14.9 MB (69.00%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -20,20 +20,34 @@ public:
         // }
         // return false;
         
-        for(int i = 0; i<matrix.size(); i++){
-            int l = 0;
-            int r = matrix[0].size()-1;
+//         for(int i = 0; i<matrix.size(); i++){
+//             int l = 0;
+//             int r = matrix[0].size()-1;
             
-            while(l<=r){
-                int m = (l+r)/2;
-                if(matrix[i][m] == target)
-                    return true;
-                if(matrix[i][m]>target)
-                    r = m-1;
-                else
-                    l = m+1;
-            }
+//             while(l<=r){
+//                 int m = (l+r)/2;
+//                 if(matrix[i][m] == target)
+//                     return true;
+//                 if(matrix[i][m]>target)
+//                     r = m-1;
+//                 else
+//                     l = m+1;
+//             }
+//         }
+//         return false;
+        int m = matrix.size();
+        int n= matrix[0].size();
+        auto i = upper_bound(matrix[0].begin(), matrix[0].end(), target) - matrix[0].begin();
+        
+    
+        
+        bool ans = false;
+        for(int j=0;j<m ;j++){
+           if( binary_search(matrix[j].begin(), matrix[j].begin()+i, target)){
+               ans = true;
+           }
         }
-        return false;
+        
+        return ans;
     }
 };
