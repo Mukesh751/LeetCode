21f62700af44faaf9a418bda840e25a2625b4a22
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-07-18 00:18:42 +0530

    Time: 16 ms (18.73%), Space: 15.1 MB (38.21%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+   int findpivot(vector<int>& nums,int start,int end,int mid){
+        if(nums[0] <= nums[nums.size()-1]) return 0;
+        while(start<end){
+            if(nums[mid]>=nums[0]) start=mid+1;
+            else end=mid;
+            mid=start+(end-start)/2;
+        }
+        return start;
+    }
+    int binarysearch(vector<int>& nums,int start,int end,int target){
+        int mid = start+(end-start)/2;
+        while(start<=end){
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target) end=mid-1;
+            else start=mid+1;
+            mid=start+(end-start)/2;
+        }
+        return -1;
+    }
+    int s(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1,mid=start+(end-start)/2;
+        int pivot = findpivot(nums,start,end,mid);
+        return max(binarysearch(nums,start,pivot-1,target),binarysearch(nums,pivot,end,target));
+    }
+    bool search(vector<int>& nums, int target) {
+       
+        map<int,int> m;
+        for(auto x : nums){
+            m[x]++;
+        }
+        nums.clear();
+        for(auto x : m){
+            nums.push_back(x.first);
+        }
+        int x = s(nums,target);
+        
+        if(x == -1)
+            return false;
+        
+        return true;
+    }
+};
