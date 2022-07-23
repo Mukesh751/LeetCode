51ec4ae1e4673eb60336e515060741eb682c395e
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-07-24 01:08:25 +0530

    Time: 821 ms (50.53%), Space: 154.2 MB (53.74%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,18 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> orderedset;
+
+class Solution {
+public:
+   
+    vector<int> countSmaller(vector<int>& nums) {
+        orderedset s;
+        vector<int>ans;
+        for(int i = nums.size() - 1 ; i >=0 ; i--){
+            s.insert(nums[i]);
+            ans.push_back(s.order_of_key(nums[i]));
+        }
+        reverse(ans.begin() ,ans.end());
+        return ans;
+    }
+};
