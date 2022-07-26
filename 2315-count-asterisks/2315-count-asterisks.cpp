ed7ac3b2044f01660bfd7f0afb9ba7ce74cf71f7
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-07-27 02:30:12 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (37.81%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int countAsterisks(string s) {
+        
+         int numofasteriks = 0;
+bool found = false;
+
+for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '*' && !found )numofasteriks++;
+
+    if (s[i] == '|' && !found)
+        found = true;
+    else if (s[i] == '|' && found) 
+        found = false;
+}
+
+return numofasteriks;
+    }
+};
