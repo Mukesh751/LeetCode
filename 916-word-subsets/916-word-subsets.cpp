a95f3ef8f00372f4fff4f905f60f10cb508267d1
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-07-30 13:44:33 +0530

    Time: 160 ms (94.61%), Space: 102.4 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -13,16 +13,19 @@ public:
         {
             vector<int> temp(26,0);
             for(char &ch : words2[i])
-                if(universal[ch-'a'] < ++temp[ch-'a']) universal[ch-'a']++;
+                if(universal[ch-'a'] < ++temp[ch-'a']) 
+                    universal[ch-'a']++;
         }
         
         for(string &str  : words1)
         {
             vector<int> freq(26,0);
-            for(char &ch : str) freq[ch-'a']++;
+            for(char &ch : str) 
+                freq[ch-'a']++;
             bool valid = true;
             for(int i=0;i<26 && valid;i++)
-                if(freq[i]<universal[i]) valid=false;
+                if(freq[i]<universal[i]) 
+                    valid=false;
             if(valid)
                 ans.emplace_back(str);
         }
