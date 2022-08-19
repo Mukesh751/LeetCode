ce461f53b1eb924629e9953bb37cbed4fcafa2de
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-08-20 01:59:49 +0530

    Time: 3 ms (88.69%), Space: 11.2 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
- int findpivot(vector<int>& nums,int start,int end,int mid){
-        if(nums[0] <= nums[nums.size()-1]) return 0;
-        while(start<end){
-            if(nums[mid]>=nums[0]) start=mid+1;
-            else end=mid;
-            mid=start+(end-start)/2;
+    vector<int> nums;
+    int target;
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        if (nums[left] <= nums[right])
+            return nums[0];
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[left]) 
+                left = mid;
+            else if (nums[mid] < nums[left])
+                right = mid;
+            else break;
         }
-        return start;
+
+        return right;
     }
-    int binarysearch(vector<int>& nums,int start,int end,int target){
-        int mid = start+(end-start)/2;
+    int bs( int start, int end){
         while(start<=end){
-            if(nums[mid]==target)
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target)
                 return mid;
-            else if(nums[mid]>target) end=mid-1;
-            else start=mid+1;
-            mid=start+(end-start)/2;
+            if(nums[mid]>target)
+                end = mid-1;
+            else
+                start = mid+1;
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1,mid=start+(end-start)/2;
-        int pivot = findpivot(nums,start,end,mid);
-        return max(binarysearch(nums,start,pivot-1,target),binarysearch(nums,pivot,end,target));
+        int start = findMin(nums);
+       // cout<<start<<endl;
+        this->nums = nums;
+        this->target = target;
+        return max(bs(0,start-1),bs(start,nums.size()-1));
     }
 };
