1327c73f2cb43956483db9445f12c3f97c1ca3c9
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2022-08-22 02:15:45 +0530

    Time: 400 ms (8.53%), Space: 34.1 MB (5.20%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,40 +1,5 @@
 class Solution {
 public:
-            vector<int> ngl(vector<int> nums)
-    {
-        vector<int> v;
-        stack<int> s;
-        for(int i = 0; i<nums.size(); i++)
-        {
-            if(s.size() == 0)
-            {
-                v.push_back(-1);
-                s.push(nums[i]);
-            }
-            else if(s.size()>0 && s.top()>nums[i])
-            {
-                v.push_back(s.top());
-                s.push(nums[i]);
-            }
-            else
-            {
-                while(s.size()>0 && s.top()<nums[i])
-                    s.pop();
-                
-                 if(s.size() == 0)
-                {
-                v.push_back(-1);
-                s.push(nums[i]);
-                }
-                else
-                {
-                v.push_back(s.top());
-                s.push(nums[i]);
-                }
-            }
-        }
-        return v;
-    }
         vector<int> ngr(vector<int> nums)
     {
         vector<int> v;
@@ -77,7 +42,7 @@ public:
         cout<<endl;
     }
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans1 = ngl(nums);
+       
         vector<int> ans2 = ngr(nums);
         
         vector<int> ans3(nums.begin(),nums.end());
@@ -86,12 +51,12 @@ public:
         debug(ans3);
         ans3 = ngr(ans3);
         
-        debug(ans1);
+        
         debug(ans2);
         debug(ans3);
         vector<int> ans;
-        //ans.push_back(ans1[0]);
-        for(int i=0; i<ans1.size(); i++){
+        
+        for(int i=0; i<ans2.size(); i++){
             ans.push_back(max(ans2[i],ans3[i]));
             if(ans[i] == INT_MIN)
                 ans[i] = -1;
